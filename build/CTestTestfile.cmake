74eedada5bb0a744dc7f04c98524e5accb024d66
DIAMANT alexandre <diamant.alexandre@gmail.com>
2023-11-12 18:04:48 +0100

    ignorer les dossiers et fichiers spécifié dans .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexandre/Projet/diamant-hamdi-cpp-boardgames
-# Build directory: /home/alexandre/Projet/diamant-hamdi-cpp-boardgames/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
